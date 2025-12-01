b997b94c49dcb3571a87fe154d0d40e8540f8e32
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-12-01 23:07:12 +0530

    Welcome

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,40 @@
-import { colors } from '@/constants/theme'
-import React from 'react'
-import { StatusBar, StyleSheet, View } from 'react-native'
-import Animated, { FadeInDown } from 'react-native-reanimated'
-const SpalshScreen
- = () => {
+import { colors } from '@/constants/theme';
+import { useRouter } from 'expo-router';
+import React, { useEffect } from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+
+const SplashScreen = () => {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      return router.replace("/(auth)/Welcome");
+
+    }, 1500)
+  }, [])
   return (
     <View style={styles.container}>
       <StatusBar barStyle={'light-content'} backgroundColor={colors.neutral900} />
-      <Animated.Image source={require('../assets/images/splashImage.png')}
-      entering={FadeInDown.duration(700).springify()}
+      <Animated.Image 
+        source={require('@/assets/images/splashImage.png')}
+        entering={FadeInDown.duration(700).springify()}
         style={styles.logo}
         resizeMode={"contain"}
       />
-     </View>
+    </View>
   )
 }
-
-export default SpalshScreen
-
-
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:colors.neutral900
-
-    },
-     logo:{
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.neutral900
+  },
+  logo:{
     height: '23%',
     aspectRatio: 1,
   }
-})
+  
+});
+export default SplashScreen
